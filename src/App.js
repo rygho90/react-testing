4025001320ef156a8c06250a8659ac6a60f14af6
Ryan <rygho90@gmail.com>
2022-10-16 14:46:01 -0400

    Finish Lesson 1

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { useState } from "react";
+
+export default function App() {
+  const [heading, setHeading] = useState("Magnificent Monkeys");
+
+  const clickHandler = () => {
+    setHeading("Radical Rhinos");
+  };
+
+  return (
+    <>
+      <button type="button" onClick={clickHandler}>
+        Click Me
+      </button>
+      <h1>{heading}</h1>
+    </>
+  );
+}
